fa471e6b66524ec669c7168536ca2e69dc2ae447
Matheus Leme Da Silva <matheuslemedasilva000@gmail.com>
2026-04-03 17:18:04 -0300

    bootloader(stage2): rework FAT interface

--- bootloader/stage2/source/console.asm
+++ bootloader/stage2/source/console.asm
@@ -4,14 +4,192 @@
 %define _CONSOLE_ASM_
 section .text
 
+;; Initializes console
+console_init:
+	;; Set text mode
+	mov ax, 3
+	int 0x10
+
+	;; Set font 8x8
+	mov ax, 0x1112
+	int 0x10
+	
+	mov word [current_cursor_x], 1
+	mov word [current_cursor_y], 1
+
+	mov word [top_corner_x], 1
+	mov word [top_corner_y], 1
+	mov word [bottom_corner_x], 78
+	mov word [bottom_corner_y], 48
+
+	push bx
+	mov bh, byte [current_attributes]
+	call clear
+	pop bx
+	
+	push ax
+	push dx
+	push cx
+
+	;; Draw borders
+	mov al, 0xC9 ;; ╔
+	mov ah, byte [current_attributes]
+	mov dl, 0
+	mov dh, 0
+	call put_char
+	
+	;; Draw top line
+	mov cx, 78
+	mov al, 0xCD ;; ═
+	mov dl, 1
+	mov dh, 0
+.line1_loop:
+	call put_char
+	inc dl
+	loop .line1_loop
+
+	mov al, 0xBB ;; ╗
+	mov dl, 79
+	mov dh, 0
+	call put_char
+
+	;; Print ║ in col 0 and 79 in lines 1-48
+	mov cx, 48
+	mov al, 0xBA ;; ║
+	mov dh, 1
+.rows_loop:
+	;; col 0
+	mov dl, 0
+	call put_char
+
+	;; col 79
+	mov dl, 79
+	call put_char
+
+	inc dh
+	loop .rows_loop
+
+	mov al, 0xC8 ;; ╚
+	mov dl, 0
+	mov dh, 49
+	call put_char
+	
+	;; Draw bottom line 
+	mov cx, 78
+	mov al, 0xCD ;; ═
+	mov dl, 1
+	mov dh, 49
+.line2_loop:
+	call put_char
+	inc dl
+	loop .line2_loop
+
+	mov al, 0xBC ;; ╝
+	mov dl, 79
+	mov dh, 49
+	call put_char
+
+	mov al, 'B'
+	mov dl, 2
+	mov dh, 0
+	call put_char
+
+	mov al, 'i'
+	inc dl
+	call put_char
+
+	mov al, 't'
+	inc dl
+	call put_char
+	
+	mov al, 'i'
+	inc dl
+	call put_char
+	
+	mov al, 'x'
+	inc dl
+	call put_char
+
+	pop cx
+	pop dx
+	pop ax
+
+	ret
+
+;; Clears the screen
+;; BH = attr
+clear:
+	push ax
+	push cx
+	push dx
+	mov ah, 0x06
+	mov al, 0
+	mov ch, byte [top_corner_y]
+	mov cl, byte [top_corner_x]
+	mov dh, byte [bottom_corner_y]
+	mov dl, byte [bottom_corner_x]
+	int 0x10
+	pop dx
+	pop cx
+	pop ax
+	ret
+
+;; Put a character in VGA mode
+;; AL: char
+;; AH: attributes
+;; DH: row
+;; DL: col
+put_char:
+	push ax
+	push dx
+	push di
+	push es
+	
+	mov byte [.row], dh
+	mov byte [.col], dl
+
+	;; offset = (line * 80 + col) * 2
+	;; char = offset
+	;; attr = offset + 1
+	mov di, 0xB800
+	mov es, di
+	mov di, word [.row]
+	mov dx, di
+
+	shl di, 6 ;; * 64
+	;; DI = line * 64
+	
+	shl dx, 4 ;; * 16
+	;; DX = line * 16
+
+	add di, dx
+	;; DI = line * 80
+
+	add di, word [.col]
+	;; DI = line * 80 + col
+
+	shl di, 1 ;; * 2
+	;; DI = (line * 80 + col) * 2
+
+	mov byte [es:di], al
+	mov byte [es:di+1], ah
+
+	pop es
+	pop di
+	pop dx
+	pop ax
+	ret
+.row: dw 0
+.col: dw 0
+
 ;; Prints a character on the screen
 print_char:
 	push ax
+	push bx
+	push cx
+	push dx
+	push es
 	
-	;; Print on VGA
-	mov ah, 0x0E
-	int 0x10
-
 	;; DEBUG: Print on serial
 	%ifdef DEBUG
 	push dx
@@ -21,6 +199,53 @@ print_char:
 	pop dx
 	%endif ;; DEBUG
 
+	cmp al, 0x0D
+	je .carriage_return
+
+	cmp al, 0x0A
+	je .inc_y
+
+	mov dl, byte [current_cursor_x]
+	mov dh, byte [current_cursor_y]
+	mov ah, byte [current_attributes]
+	call put_char
+
+	inc word [current_cursor_x]
+	
+	mov ax, word [bottom_corner_x]
+	cmp word [current_cursor_x], ax
+	jbe .no_inc_y
+.inc_y:
+	inc word [current_cursor_y]
+.carriage_return:
+	mov ax, word [top_corner_x]
+	mov word [current_cursor_x], ax
+.no_inc_y:
+	mov ax, word [bottom_corner_y]
+	cmp word [current_cursor_y], ax
+	jbe .no_scroll
+
+	mov ah, 0x06
+	mov al, 1
+	mov bh, byte [current_attributes]
+	mov ch, byte [top_corner_y]
+	mov cl, byte [top_corner_x]
+	mov dh, byte [bottom_corner_y]
+	mov dl, byte [bottom_corner_x]
+	int 0x10
+	dec word [current_cursor_y]
+.no_scroll
+
+	mov ah, 0x02
+	mov bh, 0
+	mov dh, byte [current_cursor_y]
+	mov dl, byte [current_cursor_x]
+	int 0x10
+
+	pop es
+	pop dx
+	pop cx
+	pop bx
 	pop ax
 	ret
 
@@ -115,5 +340,14 @@ print_nibble:
 	print_hex_word cx
 	pop cx
 %endmacro
+
+section .data
+current_attributes: db 0x1F
+current_cursor_x: dw 0
+current_cursor_y: dw 0
+top_corner_y: dw 0
+top_corner_x: dw 0
+bottom_corner_y: dw 0
+bottom_corner_x: dw 0
 
 %endif ;; _CONSOLE_ASM_
--- bootloader/stage2/source/main.asm
+++ bootloader/stage2/source/main.asm
@@ -18,49 +18,6 @@ jmp main
 
 section .text
 
-;; lists root dir using the FAT driver
-list_root_dir:
-	push ax
-	push dx
-	push bx
-	xor ax, ax
-	xor dx, dx
-	mov bx, 0
-	mov di, .entry
-.loop:
-	call fat_read_dir
-	jc .end
-
-	push ax
-	push si
-	push cx
-	mov si, .entry+fat_entry.name
-	mov cx, 0
-.print_loop:
-	cmp cx, 11
-	jae .print_loop.end
-	mov al, byte [si]
-	call print_char
-	inc si
-	inc cx
-	jmp .print_loop
-.print_loop.end:
-	newline
-	
-	pop cx
-	pop si
-	pop ax
-
-	inc ax
-	jmp .loop
-.end:
-	clc
-	pop bx
-	pop dx
-	pop ax
-	ret
-.entry: times fat_entry_size db 0
-
 ;; Bootloader main function
 main:
 	cli
@@ -70,15 +27,11 @@ main:
 	mov ss, ax
 	mov sp, 0x7C00
 	sti
-	newline
 
 	mov [drive], dl
-	
-	;; Set font 8x8
-	print "Setting font 8x8..."
-	mov ax, 0x1112
-	int 0x10
-	print " Ok", 0x0D, 0x0A
+	call console_init
+	print "Booting..."
+	newline
 
 	;; Initially, only floppies
 	xor dx, dx
@@ -89,9 +42,8 @@ main:
 	panic "Is not valid FAT partition"
 is_valid_fat:
 
-	call list_root_dir
-
 	call enable_a20_line
+
 	cli
 	hlt
 
