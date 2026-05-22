56dd761f759fc5c9f6ba79a447b01d20f7e985be
Matheus Leme da Silva <matheuslemedasilva000@gmail.com>
2026-05-22 09:33:30 -0300

    bootloader(stage2): start disk.asm rewrite

---
+++ bootloader/stage2/source/disk.asm
@@ -0,0 +1,93 @@
+;; disk.asm
+;; Created by Matheus Leme da Silva
+%IFNDEF DISK_ASM
+%DEFINE DISK_ASM
+BITS 16
+
+;; Initialize disk system
+;; DL: Disk Drive
+;; Returns:
+;; CF=1 if an error occours
+disk_init:
+    PUSH CX
+    PUSH DX
+    MOV BYTE [drive], DL
+    CALL disk_get_parameters
+    JC .error
+    MOV BYTE [sectors_per_track], CL
+    MOV BYTE [heads], DH
+
+    MOV BYTE [sectors_per_track+1], 0
+    MOV BYTE [heads+1], 0
+
+.end:
+    CLC
+    JMP .ret
+.error:
+    STC
+.ret:
+    POP DX
+    POP CX
+    RET
+
+;; Gets disk paramenters
+;; DL: Disk drive
+;; Returns:
+;; CL: Sectors per track
+;; DH: Heads
+;; CF=1 if an error occours
+disk_get_parameters:
+    PUSH AX
+    PUSH DX
+    MOV AH, 0x08
+    INT 0x13
+    INC DH
+    AND CL, 0x3F
+    POP DX
+    POP AX
+    RET
+
+;; Converts LBA to CHS
+;; DX:AX: LBA
+;; Returns:
+;; int13h ah=02h format:
+;;  CX = Cylinder and Sector
+;;  DH = Head
+disk_lba_to_chs:
+    PUSH BP
+    PUSH AX
+    ;; https://wiki.osdev.org/Disk_access_using_the_BIOS_(INT_13h)
+    ;; Temp = LBA / (Sectors per Track)
+    ;; Sector = (LBA % (Sectors per Track)) + 1
+    ;; Head = Temp % (Number of Heads)
+    ;; Cylinder = Temp / (Number of Heads)
+
+    ;; Temp
+    DIV WORD [sectors_per_track]
+    ;; AX = Temp
+    ;; DX = Sector - 1
+    INC DX
+    MOV BP, DX
+
+    XOR DX, DX
+    DIV WORD [heads]
+    ;; AX = Cylinder
+    ;; DX = Head
+    XOR DH, DH
+    XCHG DH, DL
+
+    MOV CX, BP
+    MOV CH, AL
+    SHR AX, 2
+    AND AX, 0xC0
+    OR CL, AL
+
+    POP AX
+    POP BP
+    RET
+
+drive:             DB 0
+sectors_per_track: DW 0
+heads:             DW 0
+
+%ENDIF ;; DISK_ASM
--- bootloader/stage2/source/main.asm
+++ bootloader/stage2/source/main.asm
@@ -3,5 +3,31 @@
 ORG 0x7E00
 BITS 16
 
-_start:
-    JMP $
+main:
+    CLI
+    XOR AX, AX
+    MOV DS, AX
+    MOV ES, AX
+    MOV SS, AX
+    MOV SP, 0x7E00
+    STI
+
+    CALL disk_init
+
+    MOV SI, start_message
+    CALL print_string
+
+
+;; Halts the system
+halt:
+    MOV SI, halted_message
+    CALL print_string
+    CLI
+    HLT
+
+%INCLUDE "console.asm"
+%INCLUDE "disk.asm"
+
+start_message:  DB `\r\nBitix Bootloader\r\n`, 0
+halted_message: DB `System is halted! Please, reboot.\r\n`, 0
+
