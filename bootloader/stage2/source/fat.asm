49abf1a4aac161c2376572321da878b8208b2bdb
Matheus Leme da Silva <matheuslemedasilva000@gmail.com>
2026-05-31 20:48:48 -0300

    bootloader(stage2): detect FAT type

--- bootloader/stage2/source/fat.asm
+++ bootloader/stage2/source/fat.asm
@@ -28,7 +28,7 @@ fat_next_cluster:
 
 
 ;; Initializes FAT system
-;; DX:AX: First sector of part
+;; AX: First sector of part
 ;; Returns: 
 ;; CF=1 if an error occours
 fat_init:
@@ -39,12 +39,14 @@ fat_init:
     PUSH ES
 
     MOV WORD[fat_first_sector], AX
-    MOV WORD[fat_first_sector+2], DX
+    XOR DX, DX
 
     XOR BX, BX
     MOV ES, BX
     MOV BX, 0x500
     CALL disk_read_sector
+    CMP WORD[ES:0x500+510], 0xAA55
+    JNE .error
 
     ;; Verify if is FAT32
     CMP WORD[ES:0x500+fat_bpb.sectors_per_fat], 0
@@ -68,18 +70,56 @@ fat_init:
     DIV BX 
     ;; AX = ((fat_bpb.root_dir_entries * 32) + 511) / 512
     MOV WORD[fat_root_dir_sectors], AX
-    ;; first_data_sector = fat_bpb.reserved_sectors + (fat_bpb.num_fat_tables * fat_bpb.sectors_per_fat) + root_dir_sectors
+    ;; first_data_sector = first_part_sector + fat_bpb.reserved_sectors + (fat_bpb.num_fat_tables * fat_bpb.sectors_per_fat) + root_dir_sectors
     ADD AX, WORD[ES:0x500+fat_bpb.reserved_sectors]
     ;; AX = fat_bpb.reserved_sectors + root_dir_sectors
-    MOV AX, CX
+    MOV CX, AX
     XOR AX, AX
     MOV AL, BYTE[ES:0x500+fat_bpb.num_fat_tables]
     MUL WORD[ES:0x500+fat_bpb.sectors_per_fat]
     ADD AX, WORD[fat_first_sector]
-    ADC DX, WORD[fat_first_sector+2]
-    ;; DX:AX = fat_bpb.reserved_sectors + root_dir_sectors + (fat_bpb.num_fat_tables * fat_bpb.sectors_per_fat)
+    ADD AX, CX
+    ;; DX:AX = first_part_sector + fat_bpb.reserved_sectors + root_dir_sectors + (fat_bpb.num_fat_tables * fat_bpb.sectors_per_fat)
     MOV WORD[fat_first_data_sector], AX
-    MOV WORD[fat_first_data_sector+2], DX
+    ;; data_sectors = fat_bpb.total_sectors - (fat_bpb.reserved_sectors + (fat_bpb.num_fat_tables * fat_bpb.sectors_per_fat) + root_dir_sectors)
+    XOR AH, AH
+    MOV AL, BYTE[ES:0x500+fat_bpb.num_fat_tables]
+    MOV BX, WORD[ES:0x500+fat_bpb.sectors_per_fat]
+    MUL BX
+    ;; DX:AX = fat_bpb.num_fat_tables * fat_bpb.sectors_per_fat
+    ADD AX, WORD[ES:0x500+fat_bpb.reserved_sectors]
+    ;; AX = fat_bpb.reserved_sectors + (fat_bpb.num_fat_tables * fat_bpb.sectors_per_fat)
+    ADD AX, WORD[fat_root_dir_sectors]
+    ;; AX = (fat_bpb.reserved_sectors + (fat_bpb.num_fat_tables * fat_bpb.sectors_per_fat)) + root_dir_sectors
+    MOV BX, WORD[ES:0x500+fat_bpb.total_sectors16]
+    SUB BX, AX
+    MOV AX, BX
+    ;; AX = fat_bpb.total_sectors - (fat_bpb.reserved_secotrs + ((fat_bpb.num_fat_tables * fat_bpb.sectors_per_fat) + root_dir_sectors))
+    ;; AX = data_sectors
+    MOV WORD[fat_data_sectors], AX
+
+    ;; total_clusters = data_sectors / fat_bpb.sectors_per_cluster
+    XOR DX, DX
+    MOV BX, WORD[ES:0x500+fat_bpb.sectors_per_cluster]
+    DIV BX
+    ;; AX = total_clusters
+
+    ;; If total_clusters < 4085: FAT12
+    ;; If total_clusters < 65525: FAT16
+    CMP AX, 4085
+    JB .fat12
+    MOV AH, 0x0E
+    MOV AL, '1'
+    INT 0x10
+    MOV AL, '6'
+    INT 0x10
+    JMP .end
+.fat12:
+    MOV AH, 0x0E
+    MOV AL, '1'
+    INT 0x10
+    MOV AL, '2'
+    INT 0x10
 
 .end:
     CLC
@@ -94,9 +134,12 @@ fat_init:
     POP AX
     RET
 
-fat_type:             DB 0
-fat_root_dir_sectors: DW 0
-fat_first_data_sector:DD 0
-fat_first_sector:     DD 0
+fat_type:                 DB 0
+fat_root_dir_sectors:     DW 0
+fat_data_sectors:         DW 0
+fat_first_data_sector:    DW 0
+fat_first_fat_sector:     DW 0
+fat_first_root_dir_sector:DW 0
+fat_first_sector:         DW 0
 
 %ENDIF ;; FAT_ASM
