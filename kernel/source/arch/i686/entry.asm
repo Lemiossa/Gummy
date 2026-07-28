aea8e3487af693ae53c3c5a13ef7d9d388e93b10
Matheus Leme da Silva <matheuslemedasilva000@gmail.com>
2026-07-28 12:14:35 -0300

    kernel: zero the BSS section

--- kernel/source/arch/i686/entry.asm
+++ kernel/source/arch/i686/entry.asm
@@ -5,6 +5,7 @@ BITS 32
 SECTION .text
 GLOBAL _start
 EXTERN kmain
+EXTERN __bss_start, __bss_end
 
 _start:
     MOV AX, 0x10
@@ -14,6 +15,13 @@ _start:
     MOV GS, AX
     MOV SS, AX
     MOV ESP, 0x7C00
+
+    ;; Zero BSS
+    XOR AL, AL
+    MOV ECX, __bss_end
+    SUB ECX, __bss_start
+    MOV EDI, __bss_start
+    REP STOSB
 
     CALL kmain
 
